9f7143166b05ed965f7cd46d7195841a195339ca
gabrielNunesKlein <gabrielgnk@outlook.com>
2020-11-27 19:30:13 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/klein/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.klein.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.klein.cursomc.domain.Categoria;
+import com.klein.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
