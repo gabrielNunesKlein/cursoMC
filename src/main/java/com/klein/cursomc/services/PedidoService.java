729a33a41bfe46968407fdddb66c371675860194
gabrielNunesKlein <gabrielgnk@outlook.com>
2020-12-04 16:42:01 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/klein/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.klein.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.klein.cursomc.domain.Pedido;
+import com.klein.cursomc.repositories.PedidoRepository;
+import com.klein.cursomc.services.execptions.ObjectNotFountException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFountException(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
