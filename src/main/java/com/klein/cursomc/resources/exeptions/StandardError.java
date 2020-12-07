e8b00019af19121654e306355a0573ebd50c86bb
gabrielNunesKlein <gabrielgnk@outlook.com>
2020-12-07 17:38:50 -0300

    Personalizando o retorno da validação

--- src/main/java/com/klein/cursomc/resources/exeptions/StandardError.java
+++ src/main/java/com/klein/cursomc/resources/exeptions/StandardError.java
@@ -1,6 +1,11 @@
 package com.klein.cursomc.resources.exeptions;
 
-public class StandardError {
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
