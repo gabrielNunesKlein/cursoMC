d27056e06cb2fd92ee8e7b9e62505f1deb8d9134
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-02-09 13:07:46 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/klein/cursomc/CursomcApplication.java
+++ src/main/java/com/klein/cursomc/CursomcApplication.java
@@ -1,12 +1,17 @@
 package com.klein.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.klein.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
+	@Autowired
+	private S3Service s3service;
 	
 	
 	public static void main(String[] args) {
@@ -15,6 +20,6 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-	
+		s3service.uploadFile("C:\\Temp\\Fotos\\MILA_KUNIS.jpg");
 	}
 }
